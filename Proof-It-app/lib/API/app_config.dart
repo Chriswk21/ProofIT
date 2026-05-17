3bfbe53fe5a7e8ae73b893e9df3354d14bf341da
Krees01 <kristama02@gmail.com>
2026-05-17 08:45:52 +0700

    Pembersihan File

--- Proof-It-app/lib/API/app_config.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/foundation.dart' show kIsWeb;
-class AppConfig {
-  static String get host => kIsWeb ? 'localhost' : '10.0.2.2';
-  static String get baseUrl => 'http://$host:3000/api';
-}
