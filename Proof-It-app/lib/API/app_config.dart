f56541a8517ba70253967967ba52b886aa8f3857
Chriswk21 <chriswk2103@gmail.com>
2026-05-25 23:56:36 +0700

    fix: support supabase anon key & update frontend production url

--- Proof-It-app/lib/API/app_config.dart
+++ Proof-It-app/lib/API/app_config.dart
@@ -1,5 +1,11 @@
-import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/foundation.dart' show kIsWeb, kReleaseMode;
+
 class AppConfig {
+  // Ganti URL ini dengan URL domain publik dari Railway Anda
+  static const String productionUrl = 'https://gregarious-playfulness-production-e7df.up.railway.app/api';
+  
   static String get host => kIsWeb ? 'localhost' : '10.0.2.2';
-  static String get baseUrl => 'http://$host:3000/api';
+  static String get localUrl => 'http://$host:3000/api';
+
+  static String get baseUrl => kReleaseMode ? productionUrl : localUrl;
 }
